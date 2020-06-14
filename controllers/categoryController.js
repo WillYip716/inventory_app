9eff4df006c1a53dee65c1ebe314b1a8a0fa01ca
WillYip716 <william.yip.716@gmail.com>
2020-06-13 17:44:12 -0700

    added in controller and views for category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,41 +1,205 @@
 var Category = require('../models/category');
+var Item = require('../models/item');
+var async = require('async');
+const validator = require('express-validator');
 
 // Display list of all categorys.
-exports.category_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: category list');
+exports.category_list = function(req, res,next) {
+    Category.find({})
+      .sort([['name', 'ascending']])
+      .exec(function (err, result) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('category_list', { title: 'Categories', categories: result });
+      });
 };
 
 // Display detail page for a specific category.
-exports.category_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: category detail: ' + req.params.id);
+exports.category_detail = function(req, res,next) {
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id)
+              .exec(callback);
+        },
+
+        category_items: function(callback) {
+            Item.find({ 'category': req.params.id })
+              .exec(callback);
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.category==null) { // No results.
+            var err = new Error('Category not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('category_detail', { title: 'Category Detail', category: results.category, category_items: results.category_items } );
+    });
 };
 
 // Display category create form on GET.
-exports.category_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category create GET');
+exports.category_create_get = function(req, res,next) {
+    res.render('category_form', { title: 'Create Category' });
 };
 
 // Handle category create on POST.
-exports.category_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category create POST');
-};
+exports.category_create_post = [
+   
+    // Validate that the name field is not empty.
+    validator.body('name', 'Category name').trim().isLength({ min: 1 }),
+    
+    // Sanitize (escape) the name field.
+    validator.sanitizeBody('name').escape(),
+  
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+  
+      // Extract the validation errors from a request.
+      const errors = validator.validationResult(req);
+  
+      // Create a category object with escaped and trimmed data.
+      var category = new Category(
+        { name: req.body.name }
+      );
+  
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form again with sanitized values/error messages.
+        res.render('category_form', { title: 'Create Category', category: category, errors: errors.array()});
+        return;
+      }
+      else {
+        // Data from form is valid.
+        // Check if category with same name already exists.
+        Category.findOne({ 'name': req.body.name })
+          .exec( function(err, found_category) {
+             if (err) { return next(err); }
+  
+             if (found_category) {
+               // category exists, redirect to its detail page.
+               res.redirect(found_category.url);
+             }
+             else {
+  
+               category.save(function (err) {
+                 if (err) { return next(err); }
+                 // category saved. Redirect to category detail page.
+                 res.redirect(category.url);
+               });
+  
+             }
+  
+           });
+      }
+    }
+  ];
 
 // Display category delete form on GET.
-exports.category_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category delete GET');
+exports.category_delete_get = function(req, res, next) {
+
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id).exec(callback)
+        },
+        item: function(callback) {
+            Item.find({ 'category': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.category==null) { // No results.
+            res.redirect('/catalog/category');
+        }
+        // Successful, so render.
+        res.render('category_delete', { title: 'Delete Category', category: results.category, category_items: results.item } );
+    });
 };
 
 // Handle category delete on POST.
-exports.category_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category delete POST');
-};
+exports.category_delete_post = function(req, res, next) {
+
+    async.parallel({
+        category: function(callback) {
+            Category.findById(req.params.id).exec(callback)
+        },
+        item: function(callback) {
+            Item.find({ 'category': req.params.id }).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.item.length>0) { // No results.
+            res.render('category_delete', { title: 'Delete Category', category: results.category, category_items: results.item } );
+            return;
+        }
+        else{
+            Category.findByIdAndRemove(req.body.categoryid, function deleteCategory(err) {
+                if (err) { return next(err); }
+                // Success - go to genres list
+                res.redirect('/catalog/categories')
+            })
+        }
+    });
+  };
 
 // Display category update form on GET.
-exports.category_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: category update GET');
+exports.category_update_get = function(req, res,next) {
+    Category.findById(req.params.id)
+        .exec(function(err,result){
+            if(err){return next(err);}
+            res.render('category_form',{title:'Category Update', category: result});
+        });
 };
 
 // Handle category update on POST.
-exports.category_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: category update POST');
-};
+exports.category_update_post = [
+   
+    // Validate that the name field is not empty.
+    validator.body('name', 'Category name').trim().isLength({ min: 1 }),
+    
+    // Sanitize (escape) the name field.
+    validator.sanitizeBody('name').escape(),
+  
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+  
+      // Extract the validation errors from a request.
+      const errors = validator.validationResult(req);
+  
+      // Create a category object with escaped and trimmed data.
+      var category = new Category(
+        { name: req.body.name,
+            _id: req.params.id }
+      );
+  
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form again with sanitized values/error messages.
+        res.render('category_form', { title: 'Create Category', category: category, errors: errors.array()});
+        return;
+      }
+      else {
+        // Data from form is valid.
+        // Check if category with same name already exists.
+        Category.findOne({ 'name': req.body.name })
+          .exec( function(err, found_category) {
+             if (err) { return next(err); }
+  
+             if (found_category) {
+               // category exists, redirect to its detail page.
+               res.redirect(found_category.url);
+             }
+             else {
+                
+                Category.findByIdAndUpdate(req.params.id,category,{}, function(err,result) {
+                    if (err) { return next(err); }
+                    // Success - go to genres list
+                    res.redirect(result.url);
+                })
+  
+             }
+  
+           });
+      }
+    }
+  ];
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,24 +1,104 @@
+var Store = require('../models/store');
 var Item = require('../models/item');
+var Category = require('../models/category');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+var async = require('async');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // Display list of all items.
-exports.item_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: item list');
+exports.item_list = function(req, res,next) {
+    Store.find({})
+      .populate('items')
+      .sort([['name', 'ascending']])
+      .exec(function (err, result) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('item_list', { title: 'Items', stores: result });
+      });
 };
 
 // Display detail page for a specific item.
-exports.item_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: item detail: ' + req.params.id);
+exports.item_detail = function(req, res,next) {
+    async.parallel({
+        store: function(callback) {
+            Store.find({ 'items': req.params.id }).exec(callback);
+        },
+        item: function(callback) {
+            Item.findById(req.params.id)
+              .exec(callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.item==null) { // No results.
+            var err = new Error('Item not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('item_detail', { title: 'Item Detail', store: results.store, item: results.item } );
+    });
 };
 
 // Display item create form on GET.
-exports.item_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create GET');
+exports.item_create_get = function(req, res,next) {
+    Category.find({})
+      .exec(function (err, result) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('item_form',{title:'Create Item',categories:result});
+      });
 };
 
 // Handle item create on POST.
-exports.item_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: item create POST');
-};
+exports.item_create_post = [
+
+    // Validate fields.
+    body('name', 'name must be specified').trim().isLength({ min: 1 }),
+    body('summary', 'summary must be specified').trim().isLength({ min: 1 }),
+    body('quantity', 'quantity must be set').trim().isNumeric({ min: 1 }),
+    body('price', 'price must be set to a decimal').trim().isCurrency(),
+    body('category', 'category must be chosen').trim().isLength({ min: 1 }),
+    
+    // Sanitize fields.
+    sanitizeBody('*').escape(),
+    
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a BookInstance object with escaped and trimmed data.
+        var item = new Item(
+          { 
+            name: req.body.name,
+            summary: req.body.summary,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            category: req.body.category,
+          });
+
+        if (!errors.isEmpty()) {
+            Category.find({})
+            .exec(function (err, result) {
+                if (err) { return next(err); }
+                //Successful, so render
+                res.render('item_form', { title: 'Create Item', item:item, categories:result, errors: errors.array()});
+                return;
+            });
+        }
+        else {
+            // Data from form is valid.
+            item.save(function (err) {
+                if (err) { return next(err); }
+                   // Successful - redirect to new item.
+                   res.redirect(item.url);
+            });
+        }
+    }
+];
 
 // Display item delete form on GET.
 exports.item_delete_get = function(req, res) {
